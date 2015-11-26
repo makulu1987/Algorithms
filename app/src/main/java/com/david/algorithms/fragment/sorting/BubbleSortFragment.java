9f327828ec6cb7266cbaa7ec5afa8e0b436c2eac
xujintian <xujintian@che08.com>
2015-11-26 15:31:12 +0800

    优化代码，添加插入排序

--- app/src/main/java/com/david/algorithms/fragment/sorting/BubbleSortFragment.java
+++ app/src/main/java/com/david/algorithms/fragment/sorting/BubbleSortFragment.java
@@ -18,80 +18,20 @@ import java.util.Random;
  * Created by xingzheng on 2015/11/25.
  */
 public class BubbleSortFragment extends BaseSortFragment {
-    int[] content;
-    List<TextView> codeViews = new ArrayList<>(7);
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        weakHandler = new WeakHandler<>(this);
+    protected String getCodeAssetName() {
+        return "bubble_sort";
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        initText();
         startSorting();
-        ViewGroup sorting_list_code_container = (ViewGroup) view.findViewById(R.id.sorting_list_code_container);
-        LayoutInflater.from(getActivity()).inflate(R.layout.view_bubble_sort_code, sorting_list_code_container, true);
-        for (int i = 0; i < sorting_list_code_container.getChildCount(); i++) {
-            codeViews.add((TextView) sorting_list_code_container.getChildAt(i));
-        }
-        codeViews.get(0).setText("for (int i = 0; i < content.length - 1; i++) {");
-        codeViews.get(1).setText("    for (int j = 0; j < content.length - i - 1; j++) {");
-        codeViews.get(2).setText("        if (content[j] > content[j + 1]) {");
-        codeViews.get(3).setText("            int temp = content[j];");
-        codeViews.get(4).setText("            content[j] = content[j + 1];");
-        codeViews.get(5).setText("            content[j + 1] = temp;");
-        codeViews.get(6).setText("         }\n" +
-                                 "     }\n" +
-                                 "}");
-    }
-
-    private void initText() {
-        content = new int[10];
-        for (int i = 0; i < 10; i++) {
-            content[i] = getRandomInt();
-        }
-        updateContent();
-    }
-
-    private int getRandomInt() {
-        int nextInt = random.nextInt(50);
-        boolean contains = false;
-        for (int aContent : content) {
-            if (aContent == nextInt) {
-                contains = true;
-                break;
-            }
-        }
-        if (contains) {
-            return getRandomInt();
-        }
-        return nextInt;
-    }
-
-    private void updateContent() {
-        for (int i = 0; i < 10; i++) {
-            PercentRelativeLayout.LayoutParams lp = (PercentRelativeLayout.LayoutParams) sortingItems.get(i).getLayoutParams();
-            lp.getPercentLayoutInfo().heightPercent = ((float) content[i]) / 50;
-            sortingItems.get(i).setLayoutParams(lp);
-        }
-    }
-
-    private void updateBack(int position) {
-        removeBack();
-        codeViews.get(position).setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
-    }
-
-    private void removeBack() {
-        for (int i = 0; i < codeViews.size(); i++) {
-            codeViews.get(i).setBackgroundColor(getResources().getColor(android.R.color.transparent));
-        }
     }
 
     private void startSorting() {
-        new AlgorithmThread() {
+        new SortAlgorithmThread() {
             @Override
             public void run() {
                 for (int i = 0; i < content.length - 1; i++) {
@@ -112,20 +52,9 @@ public class BubbleSortFragment extends BaseSortFragment {
                 }
                 weakHandler.sendEmptyMessage(7);
             }
-
-            public void updateAndSleep(int what, int time) {
-                weakHandler.sendEmptyMessage(what);
-                try {
-                    Thread.sleep(time);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
         }.start();
 
     }
-
-    private WeakHandler<BubbleSortFragment> weakHandler;
 
     @Override
     protected void handleMessageIml(Message msg) {
@@ -133,11 +62,8 @@ public class BubbleSortFragment extends BaseSortFragment {
             case 6:
                 updateContent();
                 break;
-            case 7:
-                removeBack();
-                break;
             default:
-                updateBack(msg.what);
+//                sorting_list_code_container.setSelection(msg.what);
                 break;
         }
     }
--- app/src/main/java/com/david/algorithms/fragment/sorting/SelectSortFragment.java
+++ app/src/main/java/com/david/algorithms/fragment/sorting/SelectSortFragment.java
@@ -18,94 +18,20 @@ import java.util.Random;
  * Created by xingzheng on 2015/11/25.
  */
 public class SelectSortFragment extends BaseSortFragment {
-    int[] content;
-    List<TextView> codeViews = new ArrayList<>(11);
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        weakHandler = new WeakHandler<>(this);
+    protected String getCodeAssetName() {
+        return "select_sort";
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        initText();
         startSorting();
-        ViewGroup sorting_list_code_container = (ViewGroup) view.findViewById(R.id.sorting_list_code_container);
-        LayoutInflater.from(getActivity()).inflate(R.layout.view_select_sort_code, sorting_list_code_container, true);
-        for (int i = 0; i < sorting_list_code_container.getChildCount(); i++) {
-            codeViews.add((TextView) sorting_list_code_container.getChildAt(i));
-        }
-
-        codeViews.get(0).setText("for (int i = 0; i < content.length - 1; i++) {");
-        codeViews.get(1).setText("    int selection=i;");
-        codeViews.get(2).setText("    for (int j = selection+1; j < content.length; j++) {");
-        codeViews.get(3).setText("        if (content[selection] > content[j]) {");
-        codeViews.get(4).setText("            selection=j;");
-        codeViews.get(5).setText("        }\n" +
-                                 "    }");
-        codeViews.get(6).setText("    if(selection!=i){");
-        codeViews.get(7).setText("        int temp=content[i];");
-        codeViews.get(8).setText("        content[i]=content[selection];");
-        codeViews.get(9).setText("        content[selection]=temp;");
-       codeViews.get(10).setText("    }\n" +
-                                 "}");
-    }
-
-    private void initText() {
-        content = new int[10];
-        for (int i = 0; i < 10; i++) {
-            content[i] = getRandomInt();
-        }
-        updateContent();
-    }
-
-    protected int getRandomInt() {
-        int nextInt = random.nextInt(50);
-        boolean contains = false;
-        for (int aContent : content) {
-            if (aContent == nextInt) {
-                contains = true;
-                break;
-            }
-        }
-        if (contains) {
-            return getRandomInt();
-        }
-        return nextInt;
-    }
-
-    private void updateContent() {
-        for (int i = 0; i < 10; i++) {
-            PercentRelativeLayout.LayoutParams lp = (PercentRelativeLayout.LayoutParams) sortingItems.get(i).getLayoutParams();
-            lp.getPercentLayoutInfo().heightPercent = ((float) content[i]) / 50;
-            sortingItems.get(i).setLayoutParams(lp);
-        }
-    }
-
-    private void updateBack(int position) {
-        removeBack();
-        codeViews.get(position).setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
-    }
-
-    private void removeBack() {
-        for (int i = 0; i < codeViews.size(); i++) {
-            codeViews.get(i).setBackgroundColor(getResources().getColor(android.R.color.transparent));
-        }
     }
 
     private void startSorting() {
-        new AlgorithmThread() {
-            @Override
-            public void updateAndSleep(int what, int time) {
-                weakHandler.sendEmptyMessage(what);
-                try {
-                    Thread.sleep(time);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+        new SortAlgorithmThread() {
 
             @Override
             public void run() {
@@ -138,19 +64,13 @@ public class SelectSortFragment extends BaseSortFragment {
 
     }
 
-    private WeakHandler<SelectSortFragment> weakHandler;
-
     @Override
     protected void handleMessageIml(Message msg) {
         switch (msg.what) {
             case 11:
                 updateContent();
                 break;
-            case 12:
-                removeBack();
-                break;
             default:
-                updateBack(msg.what);
                 break;
         }
     }
