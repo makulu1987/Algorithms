89e7a0d098503f0b0c1cc0a63eff77141f335a70
xujintian <xujintian@che08.com>
2015-11-27 13:44:18 +0800

    快速排序

--- app/src/main/java/com/david/algorithms/SecondActivity.java
+++ app/src/main/java/com/david/algorithms/SecondActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import com.david.algorithms.fragment.sorting.BubbleSortFragment;
 import com.david.algorithms.fragment.sorting.InsertSortFragment;
 import com.david.algorithms.fragment.sorting.MergeSortFragment;
+import com.david.algorithms.fragment.sorting.QuickSortFragment;
 import com.david.algorithms.fragment.sorting.SelectSortFragment;
 
 /**
@@ -27,7 +28,8 @@ public class SecondActivity extends AppCompatActivity {
 //        getFragmentManager().beginTransaction().add(R.id.content_container,new BubbleSortFragment(),"bubble").commit();
 //        getFragmentManager().beginTransaction().add(R.id.content_container,new SelectSortFragment(),"select").commit();
 //        getFragmentManager().beginTransaction().add(R.id.content_container,new InsertSortFragment(),"insert").commit();
-        getFragmentManager().beginTransaction().add(R.id.content_container,new MergeSortFragment(),"merge").commit();
+//        getFragmentManager().beginTransaction().add(R.id.content_container,new MergeSortFragment(),"merge").commit();
+        getFragmentManager().beginTransaction().add(R.id.content_container,new QuickSortFragment(),"quick").commit();
     }
 
 //    @Override
--- app/src/main/java/com/david/algorithms/fragment/sorting/QuickSortFragment.java
+++ app/src/main/java/com/david/algorithms/fragment/sorting/QuickSortFragment.java
@@ -1,7 +1,80 @@
 package com.david.algorithms.fragment.sorting;
 
+import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+
+import com.david.algorithms.R;
+
 /**
  * Created by xingzheng on 2015/11/25.
  */
-public class QuickSortFragment {
+public class QuickSortFragment extends BaseSortFragment {
+    @Override
+    protected String getCodeAssetName() {
+        return "quick_sort";
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        startSorting();
+        view.findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSorting();
+            }
+        });
+    }
+
+    private void startSorting() {
+        new SortAlgorithmThread() {
+            @Override
+            public void run() {
+                quickSort(content, 0, content.length - 1);
+                updateAndSleep(100, 100);
+                weakHandler.sendEmptyMessage(6);
+            }
+
+            private void quickSort(int[] array, int low, int high) {
+                if (high <= low) {
+                    return;
+                }
+                int key = array[low];
+                int j = high;
+                int i = low;
+                while (i != j) {
+                    while (array[j] >= key && i < j) {
+                        j--;
+                    }
+                    while (array[i] <= key && i < j) {
+                        i++;
+                    }
+                    if (i < j) {
+                        int temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+                }
+                array[low] = array[i];
+                array[i] = key;
+                quickSort(array, low, i - 1);
+                quickSort(array, i + 1, high);
+            }
+        }.start();
+
+    }
+
+    @Override
+    protected void handleMessageIml(Message msg) {
+        switch (msg.what) {
+            case 100:
+                updateContent();
+                break;
+            default:
+//                sorting_list_code_container.setSelection(msg.what);
+//                sortCodeListAdapter.setSelection(msg.what);
+                break;
+        }
+    }
 }
