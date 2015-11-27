371128dfc31355e7b36f73acb523e1d59ea70966
xujintian <xujintian@che08.com>
2015-11-27 09:56:16 +0800

    归并排序

--- app/src/main/java/com/david/algorithms/SecondActivity.java
+++ app/src/main/java/com/david/algorithms/SecondActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.Toolbar;
 
 import com.david.algorithms.fragment.sorting.BubbleSortFragment;
 import com.david.algorithms.fragment.sorting.InsertSortFragment;
+import com.david.algorithms.fragment.sorting.MergeSortFragment;
 import com.david.algorithms.fragment.sorting.SelectSortFragment;
 
 /**
@@ -25,7 +26,8 @@ public class SecondActivity extends AppCompatActivity {
         toolbar.setTitle(algorithmBean.name);
 //        getFragmentManager().beginTransaction().add(R.id.content_container,new BubbleSortFragment(),"bubble").commit();
 //        getFragmentManager().beginTransaction().add(R.id.content_container,new SelectSortFragment(),"select").commit();
-        getFragmentManager().beginTransaction().add(R.id.content_container,new InsertSortFragment(),"insert").commit();
+//        getFragmentManager().beginTransaction().add(R.id.content_container,new InsertSortFragment(),"insert").commit();
+        getFragmentManager().beginTransaction().add(R.id.content_container,new MergeSortFragment(),"merge").commit();
     }
 
 //    @Override
--- app/src/main/java/com/david/algorithms/fragment/sorting/MergeSortFragment.java
+++ app/src/main/java/com/david/algorithms/fragment/sorting/MergeSortFragment.java
@@ -24,50 +24,87 @@ public class MergeSortFragment extends BaseSortFragment {
             @Override
             public void run() {
                 for (int i = 1; i < content.length; i *= 2) {
+                    updateAndSleep(0,100);
                     mergeSort(content, i, content.length);
+                    updateAndSleep(1,100);
                 }
+                updateAndSleep(2,100);
                 weakHandler.sendEmptyMessage(6);
             }
 
             private void mergeSort(int[] originArray, int gap, int length) {
-                int i = 0;
+                int i;
+                updateAndSleep(3,100);
                 for (i = 0; i + 2 * gap <= length; i += 2 * gap) {
+                    updateAndSleep(4,100);
                     merge(originArray, i, i + gap - 1, i + 2 * gap - 1);
+                    updateAndSleep(5,100);
                 }
+                updateAndSleep(6,100);
                 if (i + gap < length) {
+                    updateAndSleep(7,100);
                     merge(originArray, i, i + gap - 1, length - 1);
+                    updateAndSleep(8,100);
                 }
+                updateAndSleep(9,100);
             }
 
             private void merge(int[] originArray, int low, int mid, int high) {
                 int i = low;
+                updateAndSleep(10,100);
                 int j = mid + 1;
+                updateAndSleep(11,100);
                 int k = 0;
+                updateAndSleep(12,100);
                 int tempArray[] = new int[high - low + 1];
+                updateAndSleep(13,100);
                 while (i <= mid && j <= high) {
+                    updateAndSleep(14,100);
                     if (originArray[i] < originArray[j]) {
+                        updateAndSleep(15,100);
                         tempArray[k] = originArray[i];
+                        updateAndSleep(16,100);
                         i++;
+                        updateAndSleep(17,100);
                     } else {
+                        updateAndSleep(18,100);
                         tempArray[k] = originArray[j];
+                        updateAndSleep(19,100);
                         j++;
+                        updateAndSleep(20,100);
                     }
+                    updateAndSleep(21,100);
                     k++;
+                    updateAndSleep(22,100);
                 }
+                updateAndSleep(23,100);
                 while (i <= mid) {
+                    updateAndSleep(24,100);
                     tempArray[k] = originArray[i];
+                    updateAndSleep(25,100);
                     i++;
+                    updateAndSleep(26,100);
                     k++;
+                    updateAndSleep(27,100);
                 }
+                updateAndSleep(28,100);
                 while (j <= high) {
+                    updateAndSleep(29,100);
                     tempArray[k] = originArray[j];
+                    updateAndSleep(30,100);
                     j++;
+                    updateAndSleep(31,100);
                     k++;
+                    updateAndSleep(32,100);
                 }
+                updateAndSleep(33,100);
                 for (int m = low; m <= high; m++) {
+                    updateAndSleep(34,100);
                     originArray[m] = tempArray[m - low];
+                    updateAndSleep(35,100);
                 }
-                updateAndSleep(6,100);
+                updateAndSleep(36,100);
+                updateAndSleep(100,100);
             }
 
         }.start();
@@ -77,11 +114,12 @@ public class MergeSortFragment extends BaseSortFragment {
     @Override
     protected void handleMessageIml(Message msg) {
         switch (msg.what) {
-            case 6:
+            case 100:
                 updateContent();
                 break;
             default:
-//                sorting_list_code_container.setSelection(msg.what);
+                sorting_list_code_container.setSelection(msg.what);
+                sortCodeListAdapter.setSelection(msg.what);
                 break;
         }
     }
